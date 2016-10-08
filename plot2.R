ce0cdab4aa5add1b3c40b6b2965226d643dc8d11
ooitomato <ooitomato@gmail.com>
2016-10-09 06:36:10 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -10,10 +10,8 @@ data <- read.table(text = grep("^[1.2]/2/2007", readLines(target), value = TRUE)
 # convert data and time to specific format
 data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
 data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
-# open device
+# start plotting
 png(filename = './plot2.png', width = 480, height = 480, units='px')
-# plot figure
 Sys.setlocale(category = "LC_ALL", locale = "english")
 plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
-# close device
 dev.off()
